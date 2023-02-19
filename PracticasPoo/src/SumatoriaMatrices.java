20ed527692aa7a62498948317248a803c559a7b6
Josue Lopez <jossue.avll@gmail.com>
2023-02-18 17:46:58 -0700

    Se Completo la ultima practica de sumatorias de ciclos

--- PracticasPoo/src/SumatoriaMatrices.java
+++ PracticasPoo/src/SumatoriaMatrices.java
@@ -3,13 +3,32 @@ import java.util.Scanner;
 public class SumatoriaMatrices {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] matriz = new int[3][3]; for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                matriz[i][j] = i+j;
+        int filas = 3, columnas = 3,sumMatriz;
+        int[][] matriz = new int[filas][columnas];
+        sumMatriz =0;
+        //primer ciclo para llenar la matriz y sumar las filas
+        for (int i = 0; i < filas; i++) {
+            int sumaFila = 0;
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j] = i + j;
                 System.out.println("Ingresa el valor de la posicion" + "[" + i + "][" + j + "]");
                 matriz[i][j] = sc.nextInt();
-
+                //suma de las filas
+                sumaFila += matriz[i][j];
+                //suma de todas las filas y columnas
+                sumMatriz += matriz[i][j];
             }
+
+            System.out.println("Suma de la fila " + (i + 1) + ": " + sumaFila);
+        }
+        System.out.println("Sumatoria de la matriz: " + sumMatriz);
+        //ciclo que suma las columnas
+        for (int j = 0; j <columnas; j++) {
+            int sumColumnas = 0;
+            for (int i = 0; i < filas; i++) {
+                sumColumnas += matriz[i][j];
+            }
+            System.out.println("Suma de la columna " + (j + 1) + ": " + sumColumnas);
         }
     }
 }
