5ab696552678bfa77387706e4e084f4f96d4f53a
Josue Lopez <jossue.avll@gmail.com>
2023-02-21 17:55:01 -0700

    Se corrigio la practica del numMayor

--- Swift/NumMayor.swift
+++ Swift/NumMayor.swift
@@ -1,25 +1,31 @@
 /*Escribir un programa que calcule y visualice el más grande y más pequeño de N números capturados. El valor de N se solicitará al principio del programa y 
 los numeros seran introducidos por el usuario.*/
 import Swift
+func obtenerMayorYMenorNumeros(cantidadNumeros: Int) -> (mayor: Int, menor: Int) {
+    var numN = cantidadNumeros
+    var arreglo = [Int](repeating: 0, count: numN)
+    var num = Int(readLine()!)!
+    arreglo[0] = num
+    var men = num
+    var may = num
+    for i in 1..<numN {
+        print("Introduzca un numero: ", terminator: "")
+        num = Int(readLine()!)!
+        if num < men {
+            men = num
+        }
+        arreglo[i] = num
+        if num > may {
+            may = num
+        }
+    }
+    return (mayor: may, menor: men)
+}
 print("Introduzca la cantidad de numeros a ingresar: ", terminator: "")
 var numN = Int(readLine()!)!
-var arreglo = [Int](repeating: 0, count: numN)
-print("Introduzca un numero: ", terminator: "")
-var num = Int(readLine()!)!
-arreglo[0] = num
-var men = num
-var may = num
-for i in 1..<numN {
-    print("Introduzca un numero: ", terminator: "")
-    num = Int(readLine()!)!
-    if num < men {
-        men = num
-    }
-    arreglo[i] = num
-    if num > may {
-        may = num
-    }
-}
-print("El numero mayor es: \(may)")
-print("El numero menor es : \(men)")
+var (mayor, menor) = obtenerMayorYMenorNumeros(cantidadNumeros: numN)
+print("El numero mayor es: \(mayor)")
+print("El numero menor es: \(menor)")
+
+
 
