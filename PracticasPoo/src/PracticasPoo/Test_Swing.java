c6c6511637eabc1733a2017fe8a594d4d5f1f1d8
Josue Lopez <jossue.avll@gmail.com>
2023-03-16 01:37:02 -0700

    Se agrego todo lo demas XD

--- PracticasPoo/src/PracticasPoo/Test_Swing.java
+++ PracticasPoo/src/PracticasPoo/Test_Swing.java
@@ -8,12 +8,12 @@ public class Test_Swing {
         JLabel label = new JLabel();
         ImageIcon pelon = new ImageIcon("C:\\Users\\jossu\\Desktop\\Psycogang.png");
         Border border = BorderFactory.createLineBorder(Color.GREEN,3);
-        label.setText("Nomas ando viendo grasias");
+        label.setText("Ola sam");
         label.setIcon(pelon);
         label.setHorizontalTextPosition(JLabel.CENTER);//Set text LEFT,CENTER,RIGHT
         label.setVerticalTextPosition(JLabel.TOP);
         label.setForeground(new Color(0xfaab11bb));//Cambia el color de la fuente
-        label.setFont(new Font("MV Boli",Font.PLAIN,20));//cambiar fuente y tamaño
+        label.setFont(new Font("MV Boli",Font.PLAIN,50));//cambiar fuente y tamaño
         label.setIconTextGap(100); //set gap of text to image
         label.setBackground(Color.BLACK);
         label.setOpaque(true);//Marca si la opacidad del fondo esta encendida o no
