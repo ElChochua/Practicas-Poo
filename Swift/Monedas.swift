216a95a670a81b8aa16e9f947dd8f8aafe211fcc
Josue Lopez <jossue.avll@gmail.com>
2023-03-30 13:20:15 -0700

    Se agregaron mas chingaderas digo yo

--- Swift/Monedas.swift
+++ Swift/Monedas.swift
@@ -1,45 +1,53 @@
 import Swift
+
 //💴💴Yenes 🤑🤑Pesos 💵Dolares 💶Euros
-class Convertidor{
-    public var 🤑🤑:Float
-    internal var 💵💵:Float
-    internal var 💶💶:Float
-    internal var 💴💴:Float
-    init(){
+class Convertidor {
+    public var 🤑🤑: Float
+    internal var 💵💵: Float
+    internal var 💶💶: Float
+    internal var 💴💴: Float
+    
+    init() {
         💵💵 = 18.71
         💶💶 = 19.90
         💴💴 = 0.14
         self.🤑🤑 = 0
     }
-    func convertirPesosADolares()->Float{
-        return 🤑🤑 / 💵💵
-    }
-    func convertirPesosAEuros()->Float{
-        return 🤑🤑 / 💶💶
-    }
-    func convertirPesosAYen()->Float{
-        return 🤑🤑 / 💴💴
+    
+    func convertir(monedasAConvertir: Float, aMoneda: String) -> Float {
+        switch aMoneda {
+        case "Dolares":
+            return monedasAConvertir / 💵💵
+        case "Euros":
+            return monedasAConvertir / 💶💶
+        case "Yenes":
+            return monedasAConvertir / 💴💴
+        default:
+            print("Introduce una moneda valida")
+            return 0
+        }
     }
 }
+
 let Conversor = Convertidor()
+
 print("¿Que tipo de conversion quieres realizar?")
 print("1. Pesos a Dolar")
 print("2. Pesos a Euros")
 print("3. Pesos a Yenes")
+
 var opcion = Int(readLine()!) ?? 0
-switch opcion{
-    case 1:
-        print("Introduce la cantidad de pesos a convertir")
-        Conversor.🤑🤑 = Float(readLine()!) ?? 0
-        print("El resultado de la conversion es: \(Conversor.convertirPesosADolares())")
-        case 2:
-        print("Introduce la cantidad de pesos a convertir")
-        Conversor.🤑🤑 = Float(readLine()!) ?? 0
-        print("El resultado de la conversion es: \(Conversor.convertirPesosAEuros())")
-        case 3:
-        print("Introduce la cantidad de pesos a convertir")
-        Conversor.🤑🤑 = Float(readLine()!) ?? 0
-        print("El resultado de la conversion es: \(Conversor.convertirPesosAYen())")
-        default:
-        print("Introduce una opcion valida")
+
+print("Introduce la cantidad de pesos a convertir")
+Conversor.🤑🤑 = Float(readLine()!) ?? 0
+
+switch opcion {
+case 1:
+    print("El resultado de la conversion es: \(Conversor.convertir(monedasAConvertir: Conversor.🤑🤑, aMoneda: "Dolares"))")
+case 2:
+    print("El resultado de la conversion es: \(Conversor.convertir(monedasAConvertir: Conversor.🤑🤑, aMoneda: "Euros"))")
+case 3:
+    print("El resultado de la conversion es: \(Conversor.convertir(monedasAConvertir: Conversor.🤑🤑, aMoneda: "Yenes"))")
+default:
+    print("Introduce una opcion valida")
 }
