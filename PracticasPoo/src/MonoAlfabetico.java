a176d86109d106fe0fde05332917e3b9f4dfe5f7
chochua <jossue.avll@gmail.com>
2023-05-17 22:31:28 -0700

    Se completo el sexifrado

--- PracticasPoo/src/MonoAlfabetico.java
+++ PracticasPoo/src/MonoAlfabetico.java
@@ -1,21 +1,47 @@
-import java.io.*;
 import java.util.*;
 public class MonoAlfabetico {
     public static char alfabetoNormal[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
                                            'n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    public static char alfabetoNormalMayusculas[] = {'A','B','C','D','E','F','G','H','I','J','K','L',
+                                                    'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     public static char alfabetoCifrado[] = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D',
-
-                                            'F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+                                             'F','G','H','J','K','L','Ñ','Z','X','C','V','B','N','M'};
+    public static char[] Mezclador(char[] array){
+        Random random =  new Random();
+        for(int i = 0;i<alfabetoNormal.length;i++){
+            int randomIndex = random.nextInt(array.length - i) + i;
+            char temp = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
+        return array;
+    }
+    public static char alfabetoCifradoMezclado[] = Mezclador(alfabetoCifrado);
     //Funcion para cifrar el mensaje
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);/*
+        System.out.println(Arrays.toString(alfabetoCifrado));
+        Collections.shuffle(Arrays.asList(alfabetoCifrado));
+        System.out.println(Arrays.toString(alfabetoCifrado));*/
+        String mensaje = "";
+        System.out.println("Ingrese el mensaje a cifrar");
+        mensaje = sc.nextLine();
+        mensaje = mensaje.toLowerCase();
+        System.out.println(cifrar(mensaje));
+        System.out.println(decifrar(cifrar(mensaje)));
+
+    }
     public static String cifrar(String mensaje) {
+
         String mensajeCifrado = "";
         //Se recorre el mensaje
         for (int i = 0; i < mensaje.length();i++){
             //Se recorre el alfabeto normal
             for(int j = 0; j < 26; j++){
                 //Si el caracter esta en el alfabeto normal, se agrega el caracter cifrado al mensaje
-                if(mensaje.charAt(i) == alfabetoNormal[j]){
-                    mensajeCifrado += alfabetoCifrado[j];
+                if(mensaje.charAt(i) == alfabetoNormal[j] || mensaje.charAt(i) == alfabetoNormalMayusculas[j]){
+                    mensajeCifrado += alfabetoCifradoMezclado[j];
                     break;
                 }
                 //Si el caracter es un espacio, se agrega un espacio al mensaje cifrado
@@ -35,7 +61,8 @@ public class MonoAlfabetico {
             //Se recorre el alfabeto cifrado
             for(int j = 0; j < 26; j++){
                 //Si el caracter esta en el alfabeto cifrado, se agrega el caracter normal al mensaje
-                if(mensaje.charAt(i) == alfabetoCifrado[j]){
+
+                if(mensaje.charAt(i) == alfabetoCifradoMezclado[j]){
                     mensajeDecifrado += alfabetoNormal[j];
                     break;
                 }
@@ -47,16 +74,6 @@ public class MonoAlfabetico {
             }
         }
         return mensajeDecifrado;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String mensaje = "";
-        System.out.println("Ingrese el mensaje a cifrar");
-        mensaje = sc.nextLine();
-        mensaje = mensaje.toLowerCase();
-        System.out.println(cifrar(mensaje));
-        System.out.println(decifrar(cifrar(mensaje)));
-
     }
 
 }
